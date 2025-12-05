2c14700b2440d194cee448522070cf35f9c22d54
kdemchyshyn <kdemchyshyn@kse.org.ua>
2025-12-05 16:53:43 +0200

    Done task 1

--- Task_1.py
+++ Task_1.py
@@ -1,1 +1,68 @@
 import numpy as np
+
+def printMatrix(X):
+    for element in X:
+        print(element)
+
+def svd(A):
+    U = np.zeros(shape=[A.shape[0], A.shape[0]])
+    V = np.zeros(shape=[A.shape[1], A.shape[1]])
+    E = np.zeros(shape=A.shape) # sigma matrix
+
+    # find AAt and AtA
+    left_matrix = np.dot(A, A.T)
+    right_matrix = np.dot(A.T, A)
+
+    print("Left matrix: ")
+    printMatrix(left_matrix)
+    print("Right matrix: ")
+    printMatrix(right_matrix)
+
+    # find eigenvalues and vectors
+    eigenvalues_v, eigenvectors_v = np.linalg.eig(right_matrix)
+    indexes = np.argsort(eigenvalues_v)[::-1]
+    eigenvalues_v = eigenvalues_v[indexes]
+    eigenvectors_v = eigenvectors_v[:, indexes]
+
+    eigenvalues_u, eigenvectors_u = np.linalg.eig(left_matrix)
+    indexes = np.argsort(eigenvalues_u)[::-1]
+    eigenvalues_u = eigenvalues_u[indexes]
+    eigenvectors_u = eigenvectors_u[:, indexes]
+
+    # find singulars
+    singular_nums = np.sqrt(eigenvalues_u) if len(eigenvalues_u) < len(eigenvalues_v) else np.sqrt(eigenvalues_v)
+
+    # construct matrixes
+    for i in range(len(singular_nums)):
+        E[i, i] = singular_nums[i]
+
+    print("Matrix E:")
+    printMatrix(E)
+
+    V = eigenvectors_v
+    U = eigenvectors_u
+
+    for i in range(len(singular_nums)):
+        if singular_nums[i] != 0:
+            U[:, i] = np.dot(A, V[:, i]) / singular_nums[i]
+
+    print("Matrix U:")
+    printMatrix(U)
+    print("Matrix V:")
+    printMatrix(V)
+
+    A_test = np.dot(np.dot(U, E), V.T)
+    if not np.allclose(A, A_test):
+        print("Matrixes not equal, wrong svd")
+        print("Original matrix: ")
+        printMatrix(A)
+        print("UEVt matrix: ")
+        printMatrix(A_test)
+        return None, None, None
+
+    print("Original matrix: ")
+    printMatrix(A)
+    print("UEVt matrix: ")
+    printMatrix(A_test)
+
+    return U, V, E
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import numpy as np
+
 import Task_1 as t1
 import Task_2 as t2
 
 def main():
+    # task 1
+    A = np.array([[1, 1], [0, 2]])
+    t1.svd(A)
     return 0
 
 if __name__ == '__main__':
